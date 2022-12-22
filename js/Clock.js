d17e39da93a78f3623137344fd07e75d2d7fa58a
Steven Owen <steven@9chd.com>
2022-12-23 09:54:42 +1100

    Update Clock.js

--- js/Clock.js
+++ js/Clock.js
@@ -1,30 +1,9 @@
 setInterval(showTime, 1000);
 	function showTime() {
+	const daynames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 	let time = new Date();
-	let day = time.getDay();
-		switch (new Date().getDay()) {
-			  case 0:
-			    day = "Sunday";
-			    break;
-			  case 1:
-			    day = "Monday";
-			    break;
-			  case 2:
-			     day = "Tuesday";
-			    break;
-			  case 3:
-			    day = "Wednesday";
-			    break;
-			  case 4:
-			    day = "Thursday";
-			    break;
-			  case 5:
-			    day = "Friday";
-			    break;
-			  case 6:
-			    day = "Saturday";
-		}
-
+	let day = dayname[time.getDay()];
+	
 	let hour = time.getHours();
         let min = time.getMinutes();
         let sec = time.getSeconds();
