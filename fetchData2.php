b2f7976f59a4e6a9555714e795d64febd596add2
Steven Owen <steven@9chd.com>
2022-12-25 18:16:00 +1100

    Update fetchData2.php

--- fetchData2.php
+++ fetchData2.php
@@ -15,6 +15,21 @@
 
    $result = $conn->query($sql);
 
+   if ($result->num_rows > 0) 
+      {
+         // output data of each row
+         while($row = $result->fetch_assoc()) 
+            {
+               echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+            }
+      } 
+   else 
+      {
+         echo "0 results";
+      }
+
+   $conn->close();
+
    // Works to here
 
    echo "Result successfully\n";
