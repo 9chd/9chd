3ecac4bfa4ae0d4197c759c84ebdbc0dce9c6793
Steven Owen <steven@9chd.com>
2022-12-25 18:00:19 +1100

    Update fetchData2.php

--- fetchData2.php
+++ fetchData2.php
@@ -2,6 +2,8 @@
    $dbhost = 'localhost:3036';
    $dbuser = 'root';
    $dbpass = 'rootpassword';
+
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
    
    echo "Fetched data successfully\n";
    
