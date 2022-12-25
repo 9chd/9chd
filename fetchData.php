860f6059b83b77cc06b7fd9272c0051a250ef67c
Steven Owen <steven@9chd.com>
2022-12-25 17:52:30 +1100

    Create fetchData.php

---
+++ fetchData.php
@@ -0,0 +1,30 @@
+<?php
+   $dbhost = 'localhost';
+   $dbuser = 'steven';
+   $dbpass = 'Illawarra(555)';
+   
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   $sql = 'SELECT item_id, temperature, humidity FROM studyemployee';
+   mysql_select_db('test_db');
+   $retval = mysql_query( $sql, $conn );
+   
+   if(! $retval ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   
+   while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+      echo "ID :{$row['item_id']}  <br> ".
+         "Temperature : {$row['temperature']} <br> ".
+         "Humidity : {$row['humidity']} <br> ".
+         "--------------------------------<br>";
+   }
+   
+   echo "Fetched data successfully\n";
+   
+   mysql_close($conn);
+?>
