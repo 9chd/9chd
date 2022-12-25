7a338cfe24aabaa433d5e1dc3137454177a2fac7
Steven Owen <steven@9chd.com>
2022-12-25 18:04:27 +1100

    Update fetchData2.php

--- fetchData2.php
+++ fetchData2.php
@@ -4,7 +4,11 @@
    $password = 'Illawarra(555)';
    $dbname = "9chd_home";
 
-   $conn = new mysqli($servername, $username, $password, $dbname);;
+   $conn = new mysqli($servername, $username, $password, $dbname);
+
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
    
    echo "Fetched data successfully\n";
    
