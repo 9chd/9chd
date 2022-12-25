57bcf0e2d2886b5f5945d9460e26fc70ac7e8edb
Steven Owen <steven@9chd.com>
2022-12-25 17:58:32 +1100

    Update fetchData2.php

--- fetchData2.php
+++ fetchData2.php
@@ -1,11 +1,5 @@
 <?php
-   $dbhost = 'localhost';
-   $dbuser = 'steven';
-   $dbpass = 'Illawarra(555)';
-   
-   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-   
-   
+ 
    
    echo "Fetched data successfully\n";
    
