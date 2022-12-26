602fd26089d7dff00918aaa57372d458fb59d457
Steven Owen <steven@9chd.com>
2022-12-27 09:43:29 +1100

    Update home.php

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 	render correctly.  The same happens with trying to put javascript into a php file.  In this situation
 	you wrap the javascript in a html document and use the readfile function.
 	*/
-  	echo readfile('header.html');
+  	readfile('header.html');
 	readfile('chart.html');
 
 	include('fetchData2.php');
