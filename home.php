0df3bb8389d1c6cc0134c68009e2ecaefe5d4840
Steven Owen <steven@9chd.com>
2022-12-27 09:21:28 +1100

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,7 @@
 <!DOCTYPE php>
 <?php
   readfile('header.html');
+  <div id="myChart" style="width:100%; max-width:600px; height:333px;"></div>
+	<script src="/js/drawChart.js"></script>
   include('fetchData2.php');
 ?>
