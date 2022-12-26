d11e6455b78bb3ed0ce702a63e0549e7bc0cf9bc
Steven Owen <steven@9chd.com>
2022-12-27 09:32:59 +1100

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,11 @@
 <!DOCTYPE php>
 <?php
+	/*
+	To create the homepage I've used a combination of html and php in a php file.  This si the only
+	way that you can include both these types together.  Trying to put php into a html file does not
+	render correctly.  The same happens with trying to put javascript into a php file.  In this situation
+	you wrap the javascript in a html document and use the readfile function.
+	*/
   	readfile('header.html');
 	include('fetchData2.php');
 	readfile('chart.html');
